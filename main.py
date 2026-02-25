7bed8f268767a2a2c927b0706bfe9d045865cc0f
Mani <146368211+Mani-766@users.noreply.github.com>
2026-02-25 16:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import os
 import requests
 from twilio.rest import Client
 
-OEM_Endpoint="https://api.openweathermap.org/data/2.5/forecast"
-api_key = "23f6ceb7267c08d6d50e0d478691169f"
-account_sid="AC639fe402050e14af9febe7a398bc64a4"
-auth_token="344174a0a81a573f73efac7ebf9bbb6f"
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+
+API_KEY = os.getenv("API_KEY")
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH = os.getenv("TWILIO_AUTH")
+
+if not all([API_KEY, TWILIO_SID, TWILIO_AUTH]):
+    raise ValueError("Environment variables not loaded")
 
 weather_params = {
-    "lat":12.971599,
-    "lon":77.594566,
-    "appid": api_key,
-    "cnt":4
+    "lat": 12.971599,
+    "lon": 77.594566,
+    "appid": API_KEY,
+    "cnt": 4,
 }
 
-response = requests.get(OEM_Endpoint, params=weather_params)
+response = requests.get(OWM_ENDPOINT, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 print(weather_data["list"][0]["weather"][0]["id"])
@@ -26,10 +31,11 @@ for data in weather_data["list"]:
          will_rain=True
 
 if will_rain:
-     client=Client(account_sid,auth_token)
+     client = Client(TWILIO_SID, TWILIO_AUTH)
      message=client.messages.create(
          body="It's going to rain today\n Please take a umbrella when you go outside 😊 ",
          from_="whatsapp:+14155238886",
          to="whatsapp:+91-8123354660"
      )
+
      print(message.status)
